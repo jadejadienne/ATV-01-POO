d37baad680757de90a3e690e2fd60daeeb191a59
Jadienne <117533327+jadejadienne@users.noreply.github.com>
2023-10-03 13:51:39 -0300

    Create Q2.java
    Imprima a soma de 1 até 1000.

---
+++ Q2.java
@@ -0,0 +1,15 @@
+//Imprima a soma de 1 até 1000.
+
+public class Q2{
+    public static void main(String[] args){
+
+   int soma = 0;
+
+   for(int n = 1; n <= 1000; n++){
+    soma += n;
+    }
+    System.out.println("a soama de 1 até 1000 é: "+ soma);
+    }
+}
+
+
