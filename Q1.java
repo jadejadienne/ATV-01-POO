41a12bf906310878f524e7c2508fe2492c0346cf
Jadienne <117533327+jadejadienne@users.noreply.github.com>
2023-10-03 13:37:03 -0300

    Create Q1.java
    Imprima todos os números de 150 a 300.

---
+++ Q1.java
@@ -0,0 +1,9 @@
+// Imprima todos os números de 150 a 300.
+
+    public class Q1{
+        public static void main(String[] args){
+            for(int j = 150; j <= 300; j++){
+                System.out.print(j+" ");
+            }
+        }
+    }
